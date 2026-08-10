914613250f7ac1779dd86e8484724b50e738ce31
felipe arroyo <felipevieira.arroyo@gmail.com>
2026-08-10 20:21:53 -0300

    feat: setup inicial y variables globales

---
+++ index.ts
@@ -0,0 +1,13 @@
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+const rl = readline.createInterface({ input, output });
+// 🚫 No eliminar las líneas de arriba ⬆️
+
+// ✍️ Escribe tu código aquí 👇
+    let systemName: string = "Sistema de Gestión de Inventario";
+    let version: string = "1.0.0";
+    let userName: string = "Felipe";
+
+// 🚫 No eliminar las líneas de abajo ⬇️
+rl.close();
