4ff136c132713acb101651e3a8e04654f6bd8766
Felipe <felipevieira.arroyo@gmail.com>
2026-08-12 20:38:53 -0300

    Tarea semana 8

--- index.ts
+++ index.ts
@@ -5,12 +5,12 @@ const rl = readline.createInterface({ input, output });
 // 🚫 No eliminar las líneas de arriba ⬆️
 
 // ✍️ Escribe tu código aquí 👇
-    let systemName: string = "Sistema de Gestión de Inventario";
+    /*let systemName: string = "Sistema de Gestión de Inventario";
     let version: string = "1.0.0";
     let userName: string = "Felipe";
 
     console.log(`Bienvenido al ${systemName} (versión ${version})`);
-    console.log(`Usuario: ${userName}`); 
+    console.log(`Usuario: ${userName}`); */
     
     interface task {
     id: number;
